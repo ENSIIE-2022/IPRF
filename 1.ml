c4f0fdf760b6210674287da22c5cb0d9710026c8
Tchebychev <lucas.naveteur@hotmail.fr>
2020-02-07 15:20:57 +0100

    Exercice 1 TP1

---
+++ 1.ml
@@ -0,0 +1,10 @@
+(*1.1*)
+let pi = 4. *. atan (1.) ;;
+(*1.2 -1 *)
+Prend 2 floats et sort 1 float
+(*1.2 -2 *)
+Prend 2 strings et sort 1 string
+(*1.3*)
+3. ** 11. ;;
+(*1.4*)
+"toto" ^ "tata" ;; (* Concatene 2 strings *)
