6bffecc548cac922716de41f6c40ae431bcf3000
Tchebychev <lucas.naveteur@hotmail.fr>
2020-02-07 16:35:14 +0100

    Push pour maxime

--- TP1/2.ml
+++ TP1/2.ml
@@ -3,3 +3,10 @@ Printf.printf "Bonjour"
 (* 2.2 *)
 let pseudo = read_line () ;;
 Printf.printf ("Bonjour %s\n") pseudo ;;
+(* 2.3 *)
+Scanf.scanf "%s\n" (fun s -> Printf.printf "Bonjour %s\n" s) ;;
+(* 2.4.1
+Le compilateur se sert des fichiers .cmi pour gérer les dépendances. 
+Lorsqu’il repère une dépendance vers un module Foo, il va chercher implicitement le fichier foo.cmi. S’il ne le trouve pas il échoue avec une erreur du style Unbound module Foo
+Le linker se sert des fichiers .cmo pour générer l´exécutable. Il faut préciser explicitement et dans le bon ordre les fichiers .cmo
+*)
