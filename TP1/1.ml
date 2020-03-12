2679f1782552e623a5f3f2b606e90101291c1178
Lucas <39488157+Tchebychev@users.noreply.github.com>
2020-03-12 10:56:42 +0100

    Clarification 1

--- TP1/1.ml
+++ TP1/1.ml
@@ -1,10 +1,10 @@
-(* 1.1 *)
+(* 1.1 : Définir la constante pi de type float *)
 let pi = 4. *. atan (1.) ;;
-(* 1.2 -1 *)
+(* 1.2 Type de l'opérateur ** ? *)
 Prend 2 floats et sort 1 float
-(* 1.2 -2 *)
+(* 1.2 Type de l'opérateur (^) *)
 Prend 2 strings et sort 1 string
-(* 1.3 *)
+(* 1.3 : Calcul de 3¹¹*)
 3. ** 11. ;;
-(* 1.4 *)
-"toto" ^ "tata" ;; (* Concatene 2 strings *)
+(* 1.4 : Concaténation de 2 strings *)
+"toto" ^ "tata" ;;
